4cf3c0471878585c70eb8081b5f88e1be81c2a86
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-20 19:49:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,19 +4,21 @@ class Song
   @@count = 0
   @@artist_count = {}
   @@genre_count = {}
-  
-  attr_accessor :album, :artist, :genre
-  def initialize(album="", artist="", genre="")
-    @album = album
+
+  attr_reader :name, :artist, :genre
+  def initialize(name="", artist="", genre="")
+    @name = name
     @artist = artist
     @genre = genre
 
     @@artists << @artist
     @@genres << @genre
     @@count += 1
-    
   end
 
-  
+  def artists
+    @@artists.uniq
+  end
+
 
 end
