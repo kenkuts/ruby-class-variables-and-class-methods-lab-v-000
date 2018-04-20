d1889d932265731f5266b144f20c912d50940752
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-20 19:54:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,8 @@ class Song
     @@artists << @artist
     @@genres << @genre
     @@count += 1
-    @@genre_count[@genre] ||= @@genre_count[@genre] += 1
-    @@artists[@artists] ||= @@artists[@artists] += 1
+    # @@genre_count[@genre] ||= @@genre_count[@genre] += 1
+    # @@artists[@artists] ||= @@artists[@artists] += 1
   end
 
   def self.count
