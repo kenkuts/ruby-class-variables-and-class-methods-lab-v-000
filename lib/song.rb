c3ca5f273c0f7cbe0e4d65eb8bf6a3a19cdbb6be
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-20 19:47:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,22 @@
 class Song
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+  
+  attr_accessor :album, :artist, :genre
   def initialize(album="", artist="", genre="")
     @album = album
     @artist = artist
     @genre = genre
+
+    @@artists << @artist
+    @@genres << @genre
+    @@count += 1
+    
   end
+
+  
 
 end
